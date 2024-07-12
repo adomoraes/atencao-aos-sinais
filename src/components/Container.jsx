5d7a4170ed4f5adcd55887c7a94cb44813deefdb
Eduardo Moraes <adomoraes@gmail.com>
2024-07-12 04:25:33 -0300

    fix: Renamed tags properties

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,25 +4,27 @@ export default function Container() {
 	return (
 		<>
 			<div className='bg-primary gap-5 p-4 lg:px-28 grid-cols-1 sm:grid-cols-2 grid-rows-3 grid'>
-				<div class='md:shrink-0'>
+				<div className='md:shrink-0'>
 					<img
-						class='h-full w-full object-cover md:h-full md:w-full'
+						className='h-full w-full object-cover md:h-full md:w-full'
 						src='https://webapp347768.ip-96-126-123-7.cloudezapp.io/ipsen/atencaoaossinais/assets/contentImageOquee.png'
 						alt='Modern building architecture'
 					/>
 				</div>
 				<div className='order-2 text-primary'>
-					<div class='p-2 mb-3 w-max text-2xl tracking-wide text-white bg-pink-700 font-semibold'>
+					<div className='p-2 mb-3 w-max text-2xl tracking-wide text-white bg-pink-700 font-semibold'>
 						Conheça o Câncer Renal
 					</div>
-					<h3 class='block mt-1 text-lg leading-tight font-medium'>O que é?</h3>
-					<p class='mt-2'>
+					<h3 className='block mt-1 text-lg leading-tight font-medium'>
+						O que é?
+					</h3>
+					<p className='mt-2'>
 						Doença silenciosa, o câncer renal é um dos principais tipos de tumor
 						entre os homens entre 50 e 70 anos de idade. A doença está
 						relacionada principalmente ao tabagismo, à obesidade e à hipertensão
-						arterial. <span class='referrer'>[3]</span>
+						arterial. <span className='referrer'>[3]</span>
 					</p>
-					<p class='mt-2'>
+					<p className='mt-2'>
 						No Brasil, dados do Instituto Nacional de Câncer (INCA) mostram que
 						a incidência estimada de câncer renal é de 7 a 10 casos para cada
 						100 mil habitantes. [4]
@@ -30,18 +32,20 @@ export default function Container() {
 					<a
 						href='#'
 						title='Saiba Mais'
-						class='p-2 mt-3 block w-max uppercase text-md tracking-wide text-white bg-pink-700 font-semibold hover:underline'>
+						className='p-2 mt-3 block w-max uppercase text-md tracking-wide text-white bg-pink-700 font-semibold hover:underline'>
 						Saiba Mais
 					</a>
 				</div>
 				<div className='sm:order-3 order-4 text-primary'>
-					<h3 class='block mt-1 text-lg leading-tight font-medium'>Sintomas</h3>
-					<p class='mt-2'>
+					<h3 className='block mt-1 text-lg leading-tight font-medium'>
+						Sintomas
+					</h3>
+					<p className='mt-2'>
 						É fundamental estar atento aos sintomas que, embora inespecíficos e
 						muitas vezes confundidos com sinais de outras doença, são alertas
 						muito importantes. [7]
 					</p>
-					<p class='mt-2'>
+					<p className='mt-2'>
 						A dor na região lombar apenas em um dos lados, a presença de sangue
 						na urina, dor ao urinar, massa (caroço) na lateral ou na parte
 						inferior das costas, fadiga, perda de apetite, perda de peso sem
@@ -51,34 +55,34 @@ export default function Container() {
 					<a
 						href='#'
 						title='Saiba Mais'
-						class='p-2 mt-3 block w-max uppercase text-md tracking-wide text-white bg-pink-700 font-semibold hover:underline'>
+						className='p-2 mt-3 block w-max uppercase text-md tracking-wide text-white bg-pink-700 font-semibold hover:underline'>
 						Saiba Mais
 					</a>
 				</div>
 				<div className='sm:order-4 order-3 md:shrink-0'>
 					<img
-						class='h-full w-full object-cover md:h-full md:w-full'
+						className='h-full w-full object-cover md:h-full md:w-full'
 						src='https://webapp347768.ip-96-126-123-7.cloudezapp.io/ipsen/atencaoaossinais/assets/contentImageSintomas.png'
 					/>
 				</div>
 				<div className='sm:order-5 order-6 md:shrink-0'>
 					<img
-						class='h-full w-full object-cover md:h-full md:w-full'
+						className='h-full w-full object-cover md:h-full md:w-full'
 						src='https://webapp347768.ip-96-126-123-7.cloudezapp.io/ipsen/atencaoaossinais/assets/contentImageDiagnostico.png'
 					/>
 				</div>
 				<div className='sm:order-5 order-6 text-primary'>
-					<h3 class='block mt-1 text-lg leading-tight font-medium'>
+					<h3 className='block mt-1 text-lg leading-tight font-medium'>
 						Diagnóstico
 					</h3>
-					<p class='mt-2'>
+					<p className='mt-2'>
 						Exames de sangue e urina, como ureia e creatinina, são fundamentais
 						para avaliar a saúde dos rins e no diagnóstico do câncer renal.
 						Alterações nos índices dessas substâncias podem indicar algum
 						problema renal. Em caso de suspeita, outros exames devem ser
 						solicitados pelo médico.[8]
 					</p>
-					<p class='mt-2'>
+					<p className='mt-2'>
 						Entre os principais exames para avaliar as suspeitas de câncer renal
 						estão a ultrassonografia, a tomografia computadorizada e a
 						ressonância magnética. [8]
@@ -86,21 +90,21 @@ export default function Container() {
 					<a
 						href='#'
 						title='Saiba Mais'
-						class='p-2 mt-3 block w-max uppercase text-md tracking-wide text-white bg-pink-700 font-semibold hover:underline'>
+						className='p-2 mt-3 block w-max uppercase text-md tracking-wide text-white bg-pink-700 font-semibold hover:underline'>
 						Saiba Mais
 					</a>
 				</div>
 				<div className='sm:order-7 order-8 text-primary'>
-					<h3 class='block mt-1 text-lg leading-tight font-medium'>
+					<h3 className='block mt-1 text-lg leading-tight font-medium'>
 						Tratamento
 					</h3>
-					<p class='mt-2'>
+					<p className='mt-2'>
 						Em casos iniciais, o tratamento do câncer renal costuma ser a
 						remoção do órgão por cirurgia. No entanto, se o tumor se espalhou
 						para além do rim, tratamentos adicionais podem ser recomendados,
 						incluindo radioterapia, terapias-alvo e imunoterapia.[10],[11],[12]
 					</p>
-					<p class='mt-2'>
+					<p className='mt-2'>
 						A jornada do paciente dependerá de quando o câncer for descoberto. O
 						paciente com câncer de rim tem grandes chances de sobrevida alta se
 						diagnosticado no estágio inicial. Quando o carcinoma de células
@@ -110,13 +114,13 @@ export default function Container() {
 					<a
 						href='#'
 						title='Saiba Mais'
-						class='p-2 mt-3 block w-max uppercase text-md tracking-wide text-white bg-pink-700 font-semibold hover:underline'>
+						className='p-2 mt-3 block w-max uppercase text-md tracking-wide text-white bg-pink-700 font-semibold hover:underline'>
 						Saiba Mais
 					</a>
 				</div>
 				<div className='sm:order-8 order-7 md:shrink-0'>
 					<img
-						class='h-full w-full object-cover md:h-full md:w-full'
+						className='h-full w-full object-cover md:h-full md:w-full'
 						src='https://webapp347768.ip-96-126-123-7.cloudezapp.io/ipsen/atencaoaossinais/assets/contentImageTratamento.png'
 					/>
 				</div>
