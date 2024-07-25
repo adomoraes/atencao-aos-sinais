7d83ef6392c2b0b3463ffd7f6facf26729b9c73f
Eduardo Moraes <adomoraes@gmail.com>
2024-07-25 11:11:04 -0300

    refactor: update markup for href links
    - Updated href attributes to ensure proper link structure
    - Fixed broken links and ensured all links are functional
    - Improved accessibility by adding appropriate aria labels
    - Ensured consistency in link formatting across the application

--- src/components/References.jsx
+++ src/components/References.jsx
@@ -5,69 +5,89 @@ export default function References() {
 		<>
 			<div className='bg-white text-primary gap-5 p-4 lg:px-28 grid-cols-1 grid'>
 				<div className='text-2xl tracking-wide font-semibold'>Referências</div>
-				<div className='col-lg-12 mb-5 mb-lg-20 text-sm'>
+				<div className='col-lg-12 mb-5 mb-lg-20 text-xs'>
 					<p className='mb-1'>
-						1.Kidney Education – Disponível em:
-						https://kidneyeducation.com/Portuguese/kidney-
-						structure-and-kidney-function/59
+						<a
+							href='https://kidneyeducation.com/Portuguese/kidney-
+						structure-and-kidney-function/59'
+							target='_blank'>
+							1.Kidney Education
+						</a>
 					</p>
 					<p className='mb-1'>
-						2.American Cancer Society. Kidney cancer (Adult) – renal cell
-						carcinoma. 2015. Available at:
-						http://www.cancer.org/acs/groups/cid/documents/webcontent/003107-pdf.pdf.
-						Accessed 25 April, 2016.
+						<a
+							href='http://www.cancer.org/acs/groups/cid/documents/webcontent/003107-pdf.pdf'
+							target='_blank'>
+							2.American Cancer Society. Kidney cancer (Adult) - renal cell
+							carcinoma. 2015. Accessed 25 April, 2016.
+						</a>
 					</p>
 					<p className='mb-1'>
-						3. Glossário Temático: Fatores de Proteção e Riscos para o Câncer –
-						Ministério da Saúde Disponível em:
-						https://docs.bvsalud.org/biblioref/2020/08/942860/glossario-tematico-fatores-
-						protecao-cancer.pdf
+						<a
+							href='https://docs.bvsalud.org/biblioref/2020/08/942860/glossario-tematico-fatores-
+						protecao-cancer.pdf'
+							target='_blank'>
+							3. Glossário Temático: Fatores de Proteção e Riscos para o Câncer
+							- Ministério da Saúde.
+						</a>
 					</p>
 					<p className='mb-1'>
-						4- Instituto Nacional do Câncer (INCA) – Tipos de Câncer
-						https://www.gov.br/inca/pt-br/assuntos/cancer/tipos/
+						<a
+							href='https://www.gov.br/inca/pt-br/assuntos/cancer/tipos/'
+							target='_blank'>
+							4- Instituto Nacional do Câncer (INCA) - Tipos de Câncer
+						</a>
 					</p>
 					<p className='mb-1'>
-						5- Atlas da Mortalidade por Câncer. Disponível em:
-						https://www.inca.gov.br/app/mortalidade
+						<a href='https://www.inca.gov.br/app/mortalidade' target='_blank'>
+							5- Atlas da Mortalidade por Câncer.
+						</a>
 					</p>
 					<p className='mb-1'>
-						6.GloboCan 2018. Kidney - Estimated incidence, all ages: both sexes.
+						<a
+							href='http://globocan.iarc.fr/old/summary_table_site-
+						html.asp?selection=10210&amp;title=Kidney&amp;sex=0&amp;type=0&amp;window=1&amp;america=2&amp;build=6&amp;sort=1'
+							target='_blank'>
+							6.GloboCan 2018. Kidney - Estimated incidence, all ages: both
+							sexes.
+						</a>
 					</p>
 					<p className='mb-1'>
-						Disponível em: http://globocan.iarc.fr/old/summary_table_site-
-						html.asp?selection=10210&amp;title=Kidney&amp;sex=0&amp;type=0&amp;window=1&amp;america=2&amp;build=6&amp;sort=1
+						<a
+							href='https://vencerocancer.org.br/tipos-de-cancer/cancer-de-rim-tipos-de-
+						cancer/cancer-de-rim-sintomas-2/'
+							target='_blank'>
+							7.Instituto Vencer o Câncer. Tipos de Câncer/Câncer de Rim
+						</a>
 					</p>
 					<p className='mb-1'>
-						7.. Instituto Vencer o Câncer. Tipos de Câncer/Câncer de Rim
-						Disponível em:
-						https://vencerocancer.org.br/tipos-de-cancer/cancer-de-rim-tipos-de-
-						cancer/cancer-de-rim-sintomas-2/
-					</p>
-					<p className='mb-1'>
-						8. EAU guidelines on renal cell carcinoma Disponível em: Ljungberg
-						B, Bensalah K, Bex A, et al. EAU guidelines on renal cell carcinoma:
-						2016 update. Disponível em:
-						https://uroweb.org/guideline/renal-cell-carcinoma/. Acessado em 14
+						8. EAU guidelines on renal cell carcinoma Disponível em:
+						<a
+							href='https://uroweb.org/guideline/renal-cell-carcinoma/'
+							target='_blank'>
+							Ljungberg B, Bensalah K, Bex A, et al.
+						</a>
+						EAU guidelines on renal cell carcinoma: 2016 update. Acessado em 14
 						de agosto de 2022.
 					</p>
 					<p className='mb-1'>
 						9.Delacroix SE, Wood CG, Jonasch E. Renal neoplasia. In: Taal MW,
 						Chertow GM, Marsden PA, Skorecki K, Yu ASL, et al., eds. Brenner
-						&amp; Rector’s The Kidney. 9th ed. Philadelphia, PA: Elsevier
-						Saunders; 2012:1508–1535: chap 40.
+						&amp; Rector's The Kidney. 9th ed. Philadelphia, PA: Elsevier
+						Saunders; 2012:1508-1535: chap 40.
 					</p>
 					<p className='mb-1'>
 						10.Linehan WM, Rini BI, Yang JC. Cancer of the kidney. In: DeVita
-						VT, Lawrence TS, Rosenberg SA, eds. DeVita, Hellman, and Rosenberg’s
+						VT, Lawrence TS, Rosenberg SA, eds. DeVita, Hellman, and Rosenber's
 						Cancer: Principles &amp; Practice of Oncology. 9th ed. Philadelphia,
-						PA: Wolter Kluwer; 2011:1154–1160:chap 93.
+						PA: Wolter Kluwer; 2011:1154-1160:chap 93.
 					</p>
 					<p className='mb-1'>
-						11.Noone AM et al. SEER Cancer Statistics Review, 1975-2015,
-						National Cancer Institute. Bethesda, MD,
-						https://seer.cancer.gov/csr/1975_2015/, based on November 2017 SEER
-						data submission, posted to the SEER web site, April 2018.
+						<a href='https://seer.cancer.gov/csr/1975_2015/' target='_blank'>
+							11.Noone AM et al. SEER Cancer Statistics Review, 1975-2015,
+							National Cancer Institute. Bethesda, MD, based on November 2017
+							SEER data submission, posted to the SEER web site, April 2018.
+						</a>
 					</p>
 					<p className='mb-1'>
 						12. Escudier B, Porta C, Schmidinger M, et al. Renal cell carcinoma:
