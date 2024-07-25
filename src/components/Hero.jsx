9ce0fd1b467f9edf7d2b57ef521d59d0ac2b6dbb
Eduardo Moraes <adomoraes@gmail.com>
2024-07-25 11:16:24 -0300

    style: format code with Prettier
    - Applied Prettier formatting to all source files
    - Ensured consistent code style across the project
    - Updated Prettier configuration to enforce desired code conventions
    - Fixed minor formatting issues for improved readability

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-
-export default function Hero(){
-    return(
-        <>
-
-            <section className="bg-center bg-cover bg-no-repeat bg-[url('../assets/banner.jpg')] bg-gray-700 bg-blend-multiply">
-                <div className="px-4 mx-auto max-w-2xl text-center py-24 lg:py-56">
-                <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                        Conheça os sinais do Câncer Renal
-                    </h1>
-                </div>
-            </section>
-            
-        </>
-    )
+export default function Hero() {
+	return (
+		<>
+			<section className="bg-center bg-cover bg-no-repeat bg-[url('../assets/banner.jpg')] bg-gray-700 bg-blend-multiply">
+				<div className='px-4 mx-auto max-w-2xl text-center py-24 lg:py-56'>
+					<h1 className='text-4xl font-bold tracking-tight text-white sm:text-6xl'>
+						Conheça os sinais do Câncer Renal
+					</h1>
+				</div>
+			</section>
+		</>
+	)
 }
