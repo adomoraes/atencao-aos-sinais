af14fdfbef566dbe7ee08a32cecbffe9c5447449
Eduardo Moraes <adomoraes@gmail.com>
2024-08-16 12:37:07 -0300

    build: ajustar basename para rotas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
 	return (
 		<>
-			<section className="bg-center bg-cover bg-no-repeat bg-[url('../assets/banner.jpg')] bg-gray-700 bg-blend-multiply">
+			<section className="bg-center bg-cover bg-no-repeat bg-[url('./assets/banner.jpg')] bg-gray-700 bg-blend-multiply">
 				<div className='px-4 mx-auto max-w-2xl text-center py-24 lg:py-56'>
 					<h1 className='text-4xl font-bold tracking-tight text-white sm:text-6xl'>
 						Conheça os sinais do Câncer Renal
